75bef0075d997b432bc866f55f287ef5db0838c4
AnnTumanina <98632330+aAa-AaA-aAa999@users.noreply.github.com>
2025-09-22 05:33:19 +0300

    Add files via upload

---
+++ src/test/java/org/example/Hooks.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.BeforeStep;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Hooks {
+    protected static WebDriver driver; // поле класса
+    private static String baseAddress = "http://localhost:8080/food";
+    protected static Connection connection;
+    protected static Statement statement;
+    @BeforeAll
+    public static void setUp() {
+
+            try{
+                System.setProperty("webdriver.edge.driver",
+                        "C:\\Users\\user\\Desktop\\testJava\\autotest_practice_JDBC2\\src\\test\\resources\\msedgedriver.exe");
+
+                driver = new EdgeDriver(); // присваиваем полю класса
+                driver.get(baseAddress);
+
+
+                connection = DriverManager.getConnection(
+                        "jdbc:h2:tcp://localhost:9092/mem:testdb",
+                        "user",
+                        "pass");
+
+                statement = connection.createStatement();
+
+                System.out.println("Подключение к базе установлено");
+            }
+            catch (SQLException sqlExc){
+                System.err.println("Не удалось установить подключение к базе данных: " + sqlExc.getMessage());
+            }
+            catch(Exception e) {
+                System.err.println("Ошибка при инициализации теста: " + e.getMessage());
+            }
+        }
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+
+
+        if (connection != null) {
+            try {
+                if (!connection.isClosed()) {
+                    connection.close();
+                    System.out.println("Подключение к базе закрыто");
+                }
+            } catch (SQLException sqlExc) {
+                System.err.println("Ошибка при закрытии подключения к базе: " + sqlExc.getMessage());
+            }
+        }
+    }
+}
+
