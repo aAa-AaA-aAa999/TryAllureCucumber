d65a94358f7674af6e0b68c6329ea5f4a572a480
AnnTumanina <98632330+aAa-AaA-aAa999@users.noreply.github.com>
2025-10-06 11:52:05 +0300

    Update RunnerTest.java

--- src/test/java/org/example/RunnerTest.java
+++ src/test/java/org/example/RunnerTest.java
@@ -8,7 +8,6 @@ import static io.cucumber.core.options.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/feature"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
@@ -16,6 +15,7 @@ import static io.cucumber.core.options.Constants.*;
 
 
 public class RunnerTest {
+
 
 
 }
